7135a4bc03c4d522c3cc0888687f5b4b7e977881
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-04-27 14:23:49 +0200

    feat: ajout de verification des header

--- tests/ProductApiTest.php
+++ tests/ProductApiTest.php
@@ -9,9 +9,9 @@ class ProductApiTest extends ApiTestCase
     public function testSomething(): void
     {
         $response = static::createClient()->request('GET', '/api/produits');
-        $response;
 
         $this->assertResponseIsSuccessful();
         $this->assertJsonContains(['@id' => '/api/produits']);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
     }
 }
