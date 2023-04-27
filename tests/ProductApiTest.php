1f042087098383c5584569eeeafd0249dd11d783
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-04-27 14:14:16 +0200

    fix: correction du test

--- tests/ProductApiTest.php
+++ tests/ProductApiTest.php
@@ -8,9 +8,10 @@ class ProductApiTest extends ApiTestCase
 {
     public function testSomething(): void
     {
-        $response = static::createClient()->request('GET', '/');
+        $response = static::createClient()->request('GET', '/api/produits');
+        $response;
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonContains(['@id' => '/']);
+        $this->assertJsonContains(['@id' => '/api/produits']);
     }
 }
